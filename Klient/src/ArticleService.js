7a51753649d6c650f04c93ca95d1044c66568ddb
emir derouiche <memir0@hotmail.no>
2019-11-24 22:07:36 +0100

    klient side testing skal funke nå

--- Klient/src/ArticleService.js
+++ Klient/src/ArticleService.js
@@ -1,42 +1,69 @@
-
 // @flow
-const axios = require('axios');
-var testingUrl = "http://localhost:8080" //process.env.CI ? "/localhost:80" : "";
+const axios = require("axios");
+var testingUrl = "http://localhost:8080"; //process.env.CI ? "/localhost:80" : "";
+
+export class Article {
+  overskrift: string;
+  innhold: string;
+  fultInnhold: string;
+  bilde: string;
+  bildeAlt: string;
+  kategori: number;
+  viktighet: number;
+}
+
+export class Comment {
+  innhold: string;
+  navn: string;
+}
 
 export default class ArticleService {
-    static async getArticles(kategori: Number) {
-        return axios.get(testingUrl + '/Artikler/kategori/' + kategori.toString());
-    }
+  static async getArticles(kategori: Number) {
+    return axios.get<Article[]>(
+      testingUrl + "/Artikler/kategori/" + kategori.toString()
+    );
+  }
 
-    static async getArticle(artikkelId: Number) {
-        return axios.get(testingUrl + '/Artikler/' + artikkelId.toString());
-    }
+  static async getArticle(artikkelId: Number) {
+    return axios.get<Article>(
+      testingUrl + "/Artikler/" + artikkelId.toString()
+    );
+  }
 
-    static async fjernArtikkel(artikkelId: Number) {
-        return axios.delete(testingUrl + '/Artikler/' + artikkelId.toString());
-    }
+  static async fjernArtikkel(artikkelId: Number) {
+    return axios.delete<Article, void>(
+      testingUrl + "/Artikler/" + artikkelId.toString()
+    );
+  }
 
-    static async redigerArtikkel(state: Object) {
-        return axios.post(testingUrl + '/Artikler/' + state.artikkelId.toString(), state);
-    }
+  static async redigerArtikkel(state: Object) {
+    return axios.post<Article, void>(
+      testingUrl + "/Artikler/" + state.artikkelId.toString(),
+      state
+    );
+  }
 
-    static async addArticle(state: Object) {
-        return axios.post(testingUrl + '/Artikler', state);
-    }
+  static async addArticle(state: Object) {
+    return axios.post<Article, void>(testingUrl + "/Artikler", state);
+  }
 
-    static async getAllComments() {
-        return axios.get(testingUrl + '/Kommentarer/');
-    }
+  static async getAllComments() {
+    return axios.get<Comment[]>(testingUrl + "/Kommentarer/");
+  }
 
-    static async sendLikeComment(kommentarId: number) {
-        return axios.put(testingUrl + '/Kommentarer/' + kommentarId.toString());
-    }
+  static async sendLikeComment(kommentarId: number) {
+    return axios.put<Comment, void>(
+      testingUrl + "/Kommentarer/" + kommentarId.toString()
+    );
+  }
 
-    static async sendLikeArticle(artikkelId: number) {
-        return axios.put(testingUrl + '/Artikler/Like/' + artikkelId.toString());
-    }
+  static async sendLikeArticle(artikkelId: number) {
+    return axios.put<number, void>(
+      testingUrl + "/Artikler/Like/" + artikkelId.toString()
+    );
+  }
 
-    static async sendComment(data: Object) {
-        return axios.post(testingUrl + '/Kommentarer', data);
-    }
+  static async sendComment(data: Object) {
+    return axios.post<Comment, void>(testingUrl + "/Kommentarer", data);
+  }
 }
